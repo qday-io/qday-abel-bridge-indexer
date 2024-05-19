0cfc5625d6c540968ae87d74ac2fa6d563bddef6
sunjiangjun <nihaojiangjun@163.com>
2024-05-19 13:07:20 +0800

    fix: get to address

--- pkg/aa/aa.go
+++ pkg/aa/aa.go
@@ -3,6 +3,7 @@ package aa
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/b2network/b2-indexer/pkg/log"
 	"github.com/b2network/b2-indexer/pkg/rpc"
@@ -19,8 +20,12 @@ type Response struct {
 	}
 }
 
-func GetPubKey(api, btcAddress string, network string) (*Response, error) {
-	uri := fmt.Sprintf("%v/api/bridge/toAddress?from_network=%v&from_address=%v", api, network, btcAddress)
+func GetPubKey(api, txId, btcFromAddress string, btcFromNetwork string) (*Response, error) {
+	if !strings.HasPrefix(txId, "0x") {
+		txId = fmt.Sprintf("0x%v", txId)
+	}
+
+	uri := fmt.Sprintf("%v/api/bridge/hash?hash=%v", api, txId)
 	res, err := rpc.HTTPGet(uri)
 	if err != nil {
 		return nil, err
@@ -33,10 +38,17 @@ func GetPubKey(api, btcAddress string, network string) (*Response, error) {
 	code := root.Get("code").Int()
 	msg := root.Get("message").String()
 	pubKey := root.Get("data.to_address").String()
+	fromNet := root.Get("data.from_network").String()
+	fromAddr := root.Get("data.from_address").String()
+
+	if fromNet != btcFromNetwork || fromAddr != btcFromAddress {
+		log.Warnf("bridge data error: hash:%v,btcFromNetwork:%v,btcFromAddress:%v,but onchain data: fromNetwork:%v,fromAddress:%v", txId, btcFromNetwork, btcFromAddress, fromNet, fromAddr)
+		return nil, fmt.Errorf("bridge data error: hash:%v,btcFromNetwork:%v,btcFromAddress:%v,but onchain data: fromNetwork:%v,fromAddress:%v", txId, btcFromNetwork, btcFromAddress, fromNet, fromAddr)
+	}
 
 	if code != 0 || len(pubKey) < 1 {
-		log.Warnf("not found L2 address for btcAddres:%v \n", btcAddress)
-		return nil, fmt.Errorf("not found L2 address for btcAddres:%v", btcAddress)
+		log.Warnf("not found L2 address for btcAddres:%v \n", btcFromAddress)
+		return nil, fmt.Errorf("not found L2 address for btcAddres:%v", btcFromAddress)
 	}
 
 	btcResp := Response{Code: fmt.Sprintf("%v", code), Message: msg, Data: struct{ Pubkey string }{Pubkey: pubKey}}
